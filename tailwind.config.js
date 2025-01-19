89338a24b086a0c40f28646b9c8e2acbc7ff1dcc
uhveg <ulises.1124@gmail.com>
2025-01-19 17:41:21 -0600

    Added tailwind configuration and custom colors

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "colSec-1": "#1b1b1e",
+        "colSec-2": "#B6B6B6",
+        "colSec-3": "#e0e0e2",
+        'accSec-1': '#37373D',
+        "colTxt": "#000000",
+        "primary-color": "#fa9f42",
+        "secondary-color": "#969494",
+      },
+    },
+  },
+  plugins: [],
+}
+
+
+// sandy brown fa9f42
+// charcoal 2f3e46
+// battleship gray 888f94
+// platinum e0e0e2
